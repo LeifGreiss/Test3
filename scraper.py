5f0f2a0b01775c54f02041ba43a1ff55e2cb17a2
Leif Greiss <helter1234@gmail.com>
2017-12-02 16:26:39 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,12 +2,13 @@ import scraperwiki
 import urlparse
 import lxml.html
 # scrape_table function: gets passed an individual page to scrape
+# where is root coming from?
 def scrape_table(root):
     rows = root.cssselect("table.data tr")  # selects all <tr> blocks within <table class="data">
-    for row in rows:
+    for row in rows: # where do row and rows come from? 
         # Set up our data record - we'll need it later
         record = {}
-        table_cells = row.cssselect("td")
+        table_cells = row.cssselect("td") #In the row use cssselect to select for td
         if table_cells: 
             record['Artist'] = table_cells[0].text
             record['Album'] = table_cells[1].text
@@ -16,7 +17,7 @@ def scrape_table(root):
             # Print out the data we've gathered
             print record, '------------'
             # Finally, save the record to the datastore - 'Artist' is our unique key
-            scraperwiki.datastore.save(["Artist"], record)
+            scraperwiki.sqlite.save(["Artist"], record)
         
 # scrape_and_look_for_next_link function: calls the scrape_table
 # function, then hunts for a 'next' link: if one is found, calls itself again
