c9069889d64c01e7658b67e2874f110b3541f412
Leif Greiss <helter1234@gmail.com>
2017-12-02 16:33:46 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -10,10 +10,10 @@ def scrape_table(root):
         record = {}
         table_cells = row.cssselect("td") #In the row use cssselect to select for td
         if table_cells: 
-            record['Artist'] = table_cells[0].text_content
-            record['Album'] = table_cells[1].text_content
-            record['Released'] = table_cells[2].text_content
-            record['Sales m'] = table_cells[4].text_content
+            record['Artist'] = table_cells[0].text
+            record['Album'] = table_cells[1].text
+            record['Released'] = table_cells[2].text
+            record['Sales m'] = table_cells[4].text
             # Print out the data we've gathered
             print record, '------------'
             # Finally, save the record to the datastore - 'Artist' is our unique key
